8afb50b8237037ad9eb7eadbd366bbf431513bb5
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-12 07:47:26 +0300

    add file

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTimeRemaining((time) => time - 1);
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [timeRemaining]);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -14,7 +28,7 @@ function Question({ question, onAnswered }) {
 
   return (
     <>
-      <h1>Question {id}</h1>
+      <h1>Question{id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
         const isCorrect = index === correctIndex;
